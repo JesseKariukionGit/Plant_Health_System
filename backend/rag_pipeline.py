9462201f976fac618b33c329552b46783ed9f5dc
Jesse Kariuki <muragejesse84@gmail.com>
2026-08-24 19:31:48 +0300

    Fix langchain imports for newer versions

--- backend/rag_pipeline.py
+++ backend/rag_pipeline.py
@@ -3,8 +3,8 @@ import re
 from dotenv import load_dotenv
 from pypdf import PdfReader
 from langchain_groq import ChatGroq
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema.output_parser import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain.schema import Document
